9b9511e5a992905a71da1c2a4262bdc5408c1fee
Joe <josephaddunne@gmail.com>
2024-09-30 16:17:26 +0100

    Removed redundant comment

--- langton.h
+++ langton.h
@@ -25,7 +25,4 @@ void apply_rule_general(int *colour, struct ant *ant, struct rule *rule);
 // I have changed the function signature for apply_rule_general
 // to take an int instead of a enum colour.
 
-// Anne Reinarz 24/01/2023 -
-// "For the apply_rule_general you can change the input argument types
-// as long as you keep the total number of arguments."
 #endif
